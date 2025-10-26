ac482389c01150bccd2e01ad487ae87b8f78e724
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2025-10-26 09:27:24 -0400

    refactor: added binary directory to .ignore file

--- compile.sh
+++ compile.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-# Set environment variable to enable CGO for cross-compilation
-export CGO_ENABLED=1
-
 echo "Compiling server..."
 go mod tidy
-go build -o bin/server/server ./cmd/server
+CGO_ENABLED=1 go build -o bin/server/server ./cmd/server
 echo "Compiling server... Done"
