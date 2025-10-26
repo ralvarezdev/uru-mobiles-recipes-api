a382b54e07d113bde8aff39234e5bacddcf12d88
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2025-10-25 23:29:02 -0400

    fix: set CGO_ENABLED environment variable

--- compile.sh
+++ compile.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Set environment variable to enable CGO for cross-compilation
+export CGO_ENABLED=1
+
 echo "Compiling server..."
 go mod tidy
 go build -o bin/server/server ./cmd/server
